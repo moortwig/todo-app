b7e4c0bf9db11ddfa649a3f4e4948968eedb6d80
moortwig <info@moortwig.se>
2015-06-11 01:51:16 +0200

    form validation

--- js/main.js
+++ js/main.js
@@ -1,57 +1,99 @@
+/*
+
+TODO:
+Confirmation buttons different look
+Print button
+print css
+
+Check lifetime on local storage
+Colours
+Nicer grid columns
+Code documentation
+Rearrange css code
+
+*/
 
 
-var tasks = [];
-// console.log('empty tasks', tasks);
+
+var todos = [];
+// console.log('empty todos', todos);
+
 
 $(document).ready(function() {
 	console.log('todo app ready');
-	loadTasks(tasks);
-	$.each(tasks, function(index, value) {
-		console.log(value[1]); // consolar ut description
-		$('.tasks').append("<dl><dd>Task:</dd>" + "<dt>" + value[0] + "</dt>" + "<dd>Description:</dd>" + "<dt>" + value[1] + "</dt></dl>"); 
-		$('tasks').append("<input type='checkbox' value='index' /> <hr />");
-		
+	loadTodos();
+
+	// show the Clear All button if there are stored items!
+	if (todos.length > 0) {
+		$("#clearTodoButton").show();
+	}
+
+	$.each(todos, function(index, value) {
+		// value[0] = todo title; value[1] = todo description
+		$('.todos').prepend("<dl><dd><span>" + value[0] + "</span></dd><dt>" + value[1] + "</dt>" + "</dl>"); 
 	});
 });
 
 
-
-var loadTasks = function() {
+var loadTodos = function() {
+	console.log('this loads todos');
 	if(localStorage.getItem('todo') != null) {
-		tasks = JSON.parse(localStorage.getItem('todo'));
-		console.log('loaded tasks', tasks);
+		todos = JSON.parse(localStorage.getItem('todo'));
+		console.log('loaded todos', todos);
 	} else {
-		console.log('tasks null', tasks);
-		localStorage.setItem('todo', JSON.stringify(tasks));
-		tasks = JSON.parse(localStorage.getItem('todo'));
+		console.log('todos null');
+		localStorage.setItem('todo', JSON.stringify(todos));
+		// todos = JSON.parse(localStorage.getItem('todo'));
+	}
+}
+
+// TODO change name to addTodo
+var setTodo = function() {
+	console.log('setTodo');
+
+	var singleTodo = [];
+	var newTodo = $('#todo').val();
+	var taskDescription = $('#description').val();
+
+	console.log(newTodo, taskDescription);
+
+	if (newTodo === '' || taskDescription === '') {
+		// console.log('empty string!');
+
+		$('.default-message').hide();
+		$('.error-message').show(); // tell the user what needs to be done!
+		return;
+
+	} else {
+		// console.log('description actually contains something');
+		singleTodo.push(newTodo, taskDescription); // push todo and description into a single array
+		todos.push(singleTodo); // push the array into the large array
+
+		localStorage['todo'] = JSON.stringify(todos);
+
+		$('#todo').val(''); // empty input field
+		$('#description').val(''); // empty input field
+
+		// TODO add transition to make a smoother page reload
+		location.reload();
 	}
 }
 
 
-var setTask = function() {
-	var singleTask = [];
-	var newTask = $('#task').val();
-	var taskDescription = $('#description').val();
-
-	singleTask.push(newTask, taskDescription);
-	console.log('pushed', singleTask); 		// pushed ["Testar", "litet bara"]
-	tasks.push(singleTask);
-
-	// localStorage.setItem("todo", JSON.stringify(tasks)); // overwrites!!!
-	localStorage['todo'] = JSON.stringify(tasks);
-	// localStorage.setItem('todo', tasks);
-
-
-	$('#todo-task').val(''); // empty input field
+var clearTodo = function() {
+	$('.confirm-message').show();
+	$('.button').hide();
+	$('.button-confirm').show();
 }
 
 
-var getTasks = function() {
-	// var fetched = localStorage.getItem('todo');
-	// console.log(fetched);
-	var todoList = '';
-	var todoList = JSON.parse(localStorage.getItem('todo'));
-	console.log('todoList', todoList);
-
-
+var confirmClear = function(confirmation) {
+	$('.confirm-message').hide();
+	if (confirmation === true) {
+		localStorage.removeItem('todo');
+		location.reload();
+	} else {
+		$('.button').show();
+		$('.button-confirm').hide();
+	}
 }
